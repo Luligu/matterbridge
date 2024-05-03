d2ec78ad2f0ada24c009597bd253209d383720eb
Luligu <132135057+Luligu@users.noreply.github.com>
2024-05-03 15:35:05 +0200

    Docker test

---
+++ docker/link-matterbridge-docker-script.js
@@ -0,0 +1,14 @@
+import { exec } from 'child_process';
+
+const command = 'npm link matterbridge';
+
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+  }
+});
