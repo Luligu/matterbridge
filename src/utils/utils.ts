ab8b0351a866e52fcb80ef45f2e100aee30742fa
Luligu <132135057+Luligu@users.noreply.github.com>
2024-08-28 09:11:17 +0200

    Update utils logging

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -25,7 +25,7 @@ import os from 'os';
 import { createWriteStream, statSync } from 'fs';
 import archiver, { ArchiverError, EntryData } from 'archiver';
 import path from 'path';
-import { AnsiLogger, LogLevel, TimestampFormat } from 'node-ansi-logger';
+import { AnsiLogger, idn, LogLevel, rs, TimestampFormat } from 'node-ansi-logger';
 import { glob } from 'glob';
 
 const log = new AnsiLogger({ logName: 'MatterbridgeUtils', logTimestampFormat: TimestampFormat.TIME_MILLIS, logLevel: LogLevel.INFO });
@@ -381,10 +381,10 @@ export function logInterfaces(debug = true): string | undefined {
 
   let ipv6Address: string | undefined;
   const networkInterfaces = os.networkInterfaces();
-  if (debug) log.info('Available Network Interfaces:', networkInterfaces);
+  if (debug) log.info('Available Network Interfaces:');
   for (const [interfaceName, networkInterface] of Object.entries(networkInterfaces)) {
     if (!networkInterface) break;
-    if (debug) log.info('Interface:', '\u001B[48;5;21m\u001B[38;5;255m', interfaceName, '\u001B[40;0m');
+    if (debug) log.info(`Interface: ${idn}${interfaceName}${rs}`);
     for (const detail of networkInterface) {
       if (debug) log.info('Details:', detail);
     }
@@ -495,9 +495,8 @@ export async function createZip(outputPath: string, ...sourcePaths: string[]): P
       }
     });
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     archive.on('entry', (entry: EntryData) => {
-      // log.debug(`- entry: ${entry.name}`);
+      log.debug(`- archive entry: ${entry.name}`);
     });
 
     archive.pipe(output);
@@ -512,7 +511,7 @@ export async function createZip(outputPath: string, ...sourcePaths: string[]): P
           const files = glob.sync(sourcePath.replace(/\\/g, '/'));
           log.debug(`adding files matching glob pattern: ${sourcePath}`);
           for (const file of files) {
-            // log.debug(`-glob file: ${file}`);
+            log.debug(`- glob file: ${file}`);
             archive.file(file, { name: file });
           }
         } else {
@@ -533,5 +532,3 @@ export async function createZip(outputPath: string, ...sourcePaths: string[]): P
     archive.finalize().catch(reject);
   });
 }
-
-// await createZip('output.zip', path.join('C:\\Users\\lligu\\.matterbridge'), path.join('C:\\Users\\lligu\\Matterbridge'));
