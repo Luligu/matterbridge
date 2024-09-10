0fe1bd70be8978249ee741ab332dc24de53de6be
Luligu <132135057+Luligu@users.noreply.github.com>
2024-09-10 19:45:16 +0200

    1.5.6-dev.1

--- src/matterbridge.ts
+++ src/matterbridge.ts
@@ -2719,7 +2719,9 @@ export class Matterbridge extends EventEmitter {
     this.expressApp.use(express.static(path.join(this.rootDirectory, 'frontend/build')));
 
     // Serve static files from '/static' endpoint
-    if (hasParameter('homedir')) this.expressApp.use('/static', express.static(path.join(this.rootDirectory, 'frontend/build/static')));
+    if (hasParameter('homedir')) {
+      this.expressApp.use('/static', express.static(path.join(this.rootDirectory, 'frontend/build/static')));
+    }
 
     if (!hasParameter('ssl')) {
       // Create an HTTP server and attach the express app
