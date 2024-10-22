3ddc53d77078f817b4a7227d856c9029e83fc223
Luligu <132135057+Luligu@users.noreply.github.com>
2024-10-22 23:11:19 +0200

    Dev 1.6.0-dev.15

--- src/matterbridgeWebsocket.test.ts
+++ src/matterbridgeWebsocket.test.ts
@@ -12,7 +12,7 @@ import { Matterbridge } from './matterbridge.js';
 import { wait, waiter } from './utils/utils.js';
 import WebSocket from 'ws';
 import { MatterbridgeDevice, onOffLight, onOffOutlet, onOffSwitch } from './matterbridgeDevice.js';
-import { Identify } from '@project-chip/matter.js/cluster';
+import { Identify } from '@project-chip/matter-node.js/cluster';
 import { RegisteredPlugin } from './matterbridgeTypes.js';
 
 // Default colors
