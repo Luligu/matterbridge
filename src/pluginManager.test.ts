54a36863072243ca443c66199e531147154e4877
Luligu <132135057+Luligu@users.noreply.github.com>
2024-10-29 09:44:28 +0100

    Jest pluginManager

--- src/pluginManager.test.ts
+++ src/pluginManager.test.ts
@@ -360,6 +360,7 @@ describe('PluginsManager load/start/configure/shutdown', () => {
 
     const version = await plugins.install('matterbridge-example-dynamic-platform');
     expect(version).not.toBeUndefined();
+    // eslint-disable-next-line no-console
     console.error(`Plugin installed: ${version}`);
     expect((plugins as any).log.log).toHaveBeenCalledWith(LogLevel.INFO, `Installing plugin ${plg}matterbridge-example-dynamic-platform${nf}`);
     expect((plugins as any).log.log).toHaveBeenCalledWith(LogLevel.INFO, `Installed plugin ${plg}matterbridge-example-dynamic-platform${nf}`);
