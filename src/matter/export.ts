21bfb4dd29063d982786d3220784f00eb85a59df
Luligu <target.epithet-0h@icloud.com>
2024-12-16 11:40:58 +0100

    Add exports for endpoints, devices, and behaviors in matter export module

--- src/matter/export.ts
+++ src/matter/export.ts
@@ -1,4 +1,7 @@
 export * from '@matter/main';
+export * from '@matter/main/endpoints';
+export * from '@matter/main/devices';
+export * from '@matter/main/behaviors';
 export { AttributeElement, ClusterElement, ClusterModel, CommandElement, EventElement, FieldElement } from '@matter/main/model';
 export * from '../matterbridgeDeviceTypes.js';
 export * from '../matterbridgeEndpoint.js';
