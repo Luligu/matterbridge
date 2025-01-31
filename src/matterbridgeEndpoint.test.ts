ea0f7e450815880d8038fbcd3d191c700adceaf1
Luligu <target.epithet-0h@icloud.com>
2025-01-31 12:00:40 +0100

    Jest

--- src/matterbridgeEndpoint.test.ts
+++ src/matterbridgeEndpoint.test.ts
@@ -198,7 +198,7 @@ describe('MatterbridgeEndpoint class', () => {
 
   beforeAll(async () => {
     // Create a MatterbridgeEdge instance
-    process.argv = ['node', 'matterbridge.js', '-mdnsInterface', 'Wi-Fi', '-profile', 'Jest', '-bridge', '-logger', 'info', '-matterlogger', 'info'];
+    process.argv = ['node', 'matterbridge.js', '-mdnsInterface', 'Wi-Fi', '-profile', 'JestMain', '-bridge', '-logger', 'info', '-matterlogger', 'info'];
     matterbridge = await Matterbridge.loadInstance(true);
     await matterbridge.matterStorageManager?.createContext('events')?.clearAll();
     await matterbridge.matterStorageManager?.createContext('fabrics')?.clearAll();
