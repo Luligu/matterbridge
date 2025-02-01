83ff39b96dd1d8029fb589221774a9c6c099c689
Luligu <target.epithet-0h@icloud.com>
2025-02-01 18:14:33 +0100

    Jest

--- src/matterbridgeEndpoint-default.test.ts
+++ src/matterbridgeEndpoint-default.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { jest } from '@jest/globals';
