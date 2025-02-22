c0775a62e557573170cd2ddbf72a6640c4266567
Luligu <target.epithet-0h@icloud.com>
2025-02-22 18:39:18 +0100

    Cleanup Test page

--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
+
 // React
 import React, { useContext, useEffect, useState, useRef } from 'react';
 
@@ -11,7 +11,6 @@ import React, { useContext, useEffect, useState, useRef } from 'react';
 import { WebSocketContext } from './WebSocketProvider';
 import { UiContext } from './UiProvider';
 import { Connecting } from './Connecting';
-import { SystemInfoTable } from './SystemInfoTable';
 // import { debug } from '../App';
 const debug = true;
 
@@ -22,13 +21,13 @@ function Test() {
   const { showSnackbarMessage } = useContext(UiContext);
 
   // Local states
-  const [settings, setSettings] = useState(null);
-  const [plugins, setPlugins] = useState([]);
-  const [devices, setDevices] = useState([]);
-  const [clusters, setClusters] = useState([]);
-  const [cpu, setCpu] = useState(null);
-  const [memory, setMemory] = useState(null);
-  const [uptime, setUptime] = useState(null);
+  const [_settings, setSettings] = useState(null);
+  const [_plugins, setPlugins] = useState([]);
+  const [_devices, setDevices] = useState([]);
+  const [_clusters, setClusters] = useState([]);
+  const [_cpu, setCpu] = useState(null);
+  const [_memory, setMemory] = useState(null);
+  const [_uptime, setUptime] = useState(null);
   const uniqueId = useRef(null);
 
   if(!uniqueId.current) {
@@ -48,23 +47,23 @@ function Test() {
         if (msg.method === 'refresh_required') {
           if(debug) console.log('Test received refresh_required');
           showSnackbarMessage('Refresh required', 0);
-          sendMessage({ method: "/api/settings", src: "Frontend", dst: "Matterbridge", params: {} });
-          sendMessage({ method: "/api/plugins", src: "Frontend", dst: "Matterbridge", params: {} });
-          sendMessage({ method: "/api/devices", src: "Frontend", dst: "Matterbridge", params: {} });
+          sendMessage({ id: uniqueId.current, method: "/api/settings", src: "Frontend", dst: "Matterbridge", params: {} });
+          sendMessage({ id: uniqueId.current, method: "/api/plugins", src: "Frontend", dst: "Matterbridge", params: {} });
+          sendMessage({ id: uniqueId.current, method: "/api/devices", src: "Frontend", dst: "Matterbridge", params: {} });
         }
         if (msg.method === 'memory_update') {
           if(debug) console.log('Test received memory_update', msg);
-          showSnackbarMessage('Test received memory_update');
+          // showSnackbarMessage('Test received memory_update');
           setMemory(msg.params);
         }
         if (msg.method === 'cpu_update') {
           if(debug) console.log('Test received cpu_update', msg);
-          showSnackbarMessage('Test received cpu_update');
+          // showSnackbarMessage('Test received cpu_update');
           setCpu(msg.params);
         }
         if (msg.method === 'uptime_update') {
           if(debug) console.log('Test received uptime_update', msg);
-          showSnackbarMessage('Test received uptime_update');
+          // showSnackbarMessage('Test received uptime_update');
           setUptime(msg.params);
         }
         if (msg.method === '/api/settings' && msg.response) {
@@ -88,6 +87,7 @@ function Test() {
         }
         if (msg.method === '/api/clusters') {
           if(debug) console.log(`Test received ${msg.response.length} clusters for device ${msg.deviceName} endpoint ${msg.endpoint}:`, msg);
+          showSnackbarMessage('Test received /api/clusters');
           setClusters(msg.response);
         }
       } else {
@@ -126,28 +126,10 @@ function Test() {
   }
   return (
     <div className="MbfPageDiv" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-
-      {/* <div style={{ display: 'flex', gap: '20px', width: '100%' }}>
-        {settings && settings.systemInformation && <SystemInfoTable systemInfo={settings.systemInformation} compact={false}/>}
-      </div> */}
-
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px', width: '100%' }}>
-        <img src="matterbridge.svg" alt="Matterbridge Logo" style={{ height: '64px', width: '64px' }} />
+        <img src="matterbridge.svg" alt="Matterbridge Logo" style={{ height: '256px', width: '256px' }} />
         <p>Welcome to the Test page of the Matterbridge frontend</p>
-        {cpu && memory &&
-          <div>
-            <p>cpuUsage: {cpu.cpuUsage.toFixed(2) + ' %'}</p>
-            <p>systemUptime: {uptime.systemUptime}</p>
-            <p>processUptime: {uptime.processUptime}</p>
-            <p>freeMemory {memory.freeMemory}</p>
-            <p>totalMemory {memory.totalMemory}</p>
-            <p>rss {memory.rss}</p>
-            <p>heapUsed {memory.heapUsed}</p>
-            <p>heapTotal {memory.heapTotal}</p>
-          </div>
-        }
       </div>  
-
     </div>
   );
 }
