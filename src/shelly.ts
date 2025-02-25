fe05bf18fc19a6cd98489805007cec313e1c3b68
Luligu <target.epithet-0h@icloud.com>
2025-02-25 12:43:12 +0100

    Add network configuration and reboot functions to Shelly API

--- src/shelly.ts
+++ src/shelly.ts
@@ -1,4 +1,28 @@
+/**
+ * This file contains the shelly api functions.
+ *
+ * @file shelly.ts
+ * @author Luca Liguori
+ * @date 2025-02-19
+ * @version 1.0.3
+ *
+ * Copyright 2025, 2026, 2027 Luca Liguori.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. *
+ */
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { WS_ID_SHELLY_MAIN_UPDATE, WS_ID_SHELLY_SYS_UPDATE } from './frontend.js';
 import { debugStringify } from './logger/export.js';
 import { Matterbridge } from './matterbridge.js';
@@ -10,7 +34,7 @@ import { Matterbridge } from './matterbridge.js';
  * @returns {Promise<void>} A promise that resolves when the operation is complete.
  */
 export async function getShellySysUpdate(matterbridge: Matterbridge): Promise<void> {
-  fetchShelly('/api/updates/sys/check', 60 * 1000)
+  getShelly('/api/updates/sys/check', 60 * 1000)
     .then(async (data: { name: string }[]) => {
       if (data.length > 0) {
         matterbridge.matterbridgeInformation.shellySysUpdate = true;
@@ -34,7 +58,7 @@ export async function getShellySysUpdate(matterbridge: Matterbridge): Promise<vo
  * @returns {Promise<void>} A promise that resolves when the operation is complete.
  */
 export async function triggerShellySysUpdate(matterbridge: Matterbridge): Promise<void> {
-  fetchShelly('/api/updates/sys/perform', 10 * 1000)
+  getShelly('/api/updates/sys/perform', 10 * 1000)
     .then(async () => {
       // {"updatingInProgress":true} or {"updatingInProgress":false}
       matterbridge.log.debug(`Triggered Shelly system updates`);
@@ -57,7 +81,7 @@ export async function triggerShellySysUpdate(matterbridge: Matterbridge): Promis
  * @returns {Promise<void>} A promise that resolves when the operation is complete.
  */
 export async function getShellyMainUpdate(matterbridge: Matterbridge): Promise<void> {
-  fetchShelly('/api/updates/main/check', 60 * 1000)
+  getShelly('/api/updates/main/check', 60 * 1000)
     .then(async (data: { name: string }[]) => {
       if (data.length > 0) {
         matterbridge.matterbridgeInformation.shellyMainUpdate = true;
@@ -80,7 +104,7 @@ export async function getShellyMainUpdate(matterbridge: Matterbridge): Promise<v
  * @returns {Promise<void>} A promise that resolves when the operation is complete.
  */
 export async function triggerShellyMainUpdate(matterbridge: Matterbridge): Promise<void> {
-  fetchShelly('/api/updates/main/perform', 10 * 1000)
+  getShelly('/api/updates/main/perform', 10 * 1000)
     .then(async () => {
       // {"updatingInProgress":true} or {"updatingInProgress":false}
       matterbridge.log.debug(`Triggered Shelly main updates`);
@@ -97,7 +121,59 @@ export async function triggerShellyMainUpdate(matterbridge: Matterbridge): Promi
 }
 
 /**
- * Perform a call to Shelly board apis.
+ * Triggers Shelly change network configuration.
+ * @param {Matterbridge} matterbridge - The Matterbridge instance.
+ * @param {object} config - The network configuration.
+ * @returns {Promise<void>} A promise that resolves when the operation is complete.
+ */
+export async function triggerShellyChangeIp(matterbridge: Matterbridge, config: { type: 'static' | 'dhcp'; ip: string; subnet: string; gateway: string; dns: string }): Promise<void> {
+  const api = config.type === 'static' ? '/api/network/connection/static' : '/api/network/connection/dynamic';
+  const data: { interface: string; addr?: string; mask?: string; gw?: string; dns?: string } = { interface: 'end0' };
+  if (config.type === 'static') {
+    data['addr'] = config.ip;
+    data['mask'] = config.subnet;
+    data['gw'] = config.gateway;
+    data['dns'] = config.dns;
+  }
+
+  matterbridge.log.debug(`Triggering Shelly network configuration change: ${debugStringify(config)}`);
+
+  postShelly(api, data, 60 * 1000)
+    .then(async () => {
+      matterbridge.log.debug(`Triggered Shelly network configuration change: ${debugStringify(config)}`);
+    })
+    .catch((error) => {
+      matterbridge.log.debug(`****Error triggering Shelly network configuration change: ${error instanceof Error ? error.message : error}`);
+    })
+    .finally(() => {
+      matterbridge.log.notice(`Changed Shelly network configuration`);
+      matterbridge.frontend.wssSendSnackbarMessage('Changed Shelly network configuration');
+    });
+}
+
+/**
+ * Triggers Shelly system reboot.
+ * @param {Matterbridge} matterbridge - The Matterbridge instance.
+ * @returns {Promise<void>} A promise that resolves when the operation is complete.
+ */
+export async function triggerShellyReboot(matterbridge: Matterbridge): Promise<void> {
+  matterbridge.log.debug(`Triggering Shelly system reboot`);
+
+  postShelly('/api/system/reboot', {}, 60 * 1000)
+    .then(async () => {
+      matterbridge.log.debug(`Triggered Shelly system reboot`);
+    })
+    .catch((error) => {
+      matterbridge.log.debug(`****Error triggering Shelly system reboot: ${error instanceof Error ? error.message : error}`);
+    })
+    .finally(() => {
+      matterbridge.log.notice(`Rebooting Shelly board...`);
+      matterbridge.frontend.wssSendSnackbarMessage('Rebooting Shelly board...');
+    });
+}
+
+/**
+ * Perform a GET to Shelly board apis.
  * @param {string} api - The api to call:
  *
  *     /api/updates/sys/check => [{name:string; ...}]
@@ -107,13 +183,11 @@ export async function triggerShellyMainUpdate(matterbridge: Matterbridge): Promi
  *     /api/updates/main/perform => {"updatingInProgress":true} or {"updatingInProgress":false}
  *     /api/updates/main/status  => {"updatingInProgress":true} or {"updatingInProgress":false}
  *
- *     curl -X POST http://127.0.0.1:8101/api/system/reboot => {"success":true}
- *
  * @param {number} [timeout=5000] - The timeout duration in milliseconds (default is 60000ms).
  * @returns {Promise<any>} A promise that resolves to the response.
  * @throws {Error} If the request fails.
  */
-async function fetchShelly(api: string, timeout = 60000): Promise<any> {
+async function getShelly(api: string, timeout = 60000): Promise<any> {
   const http = await import('node:http');
   return new Promise((resolve, reject) => {
     const url = `http://127.0.0.1:8101${api}`;
@@ -156,11 +230,91 @@ async function fetchShelly(api: string, timeout = 60000): Promise<any> {
   });
 }
 
+/**
+ * Perform a POST request to Shelly board apis.
+ * @param {string} api - The api to call:
+ *
+ *     Set static ip
+ *     /api/network/connection/static -d '{"interface": "end0", "addr": "10.11.12.101", "mask": "255.255.255.0", "gw": "10.11.12.1", "dns": "1.1.1.1"}' => {}
+ *
+ *     Set dhcp
+ *     /api/network/connection/dynamic -d '{"interface": "end0"}' => {}
+ *
+ *     Reboot
+ *     /api/system/reboot => {"success":true}
+ *
+ *     curl -H "Content-Type: application/json" -X POST http://127.0.0.1:8101/api/network/connection/dynamic
+ *        -d '{"interface": "end0"}'
+ *
+ *     curl -H "Content-Type: application/json" -X POST http://127.0.0.1:8101/api/network/connection/static
+ *        -d '{"interface": "end0", "addr": "192.168.1.64", "mask": "255.255.255.0", "gw": "192.168.1.1", "dns": "192.168.1.1"}'
+ *
+ * @param {number} [timeout=5000] - The timeout duration in milliseconds (default is 60000ms).
+ * @returns {Promise<any>} A promise that resolves to the response.
+ * @throws {Error} If the request fails.
+ */
+async function postShelly(api: string, data: any, timeout = 60000): Promise<any> {
+  const http = await import('node:http');
+  return new Promise((resolve, reject) => {
+    const url = `http://127.0.0.1:8101${api}`;
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => {
+      controller.abort();
+      reject(new Error(`Request timed out after ${timeout / 1000} seconds`));
+    }, timeout).unref();
+
+    const jsonData = JSON.stringify(data);
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(jsonData),
+      },
+      signal: controller.signal,
+    };
+
+    const req = http.request(url, options, (res) => {
+      let responseData = '';
+
+      // Check for non-success status codes (e.g., 300+)
+      if (res.statusCode && res.statusCode >= 300) {
+        clearTimeout(timeoutId);
+        res.resume(); // Discard response data to free up memory
+        req.destroy(); // Close the request
+        return reject(new Error(`Failed to post data. Status code: ${res.statusCode}`));
+      }
+
+      res.on('data', (chunk) => {
+        responseData += chunk;
+      });
+
+      res.on('end', () => {
+        clearTimeout(timeoutId);
+        try {
+          const jsonResponse = JSON.parse(responseData);
+          resolve(jsonResponse);
+        } catch (err) {
+          reject(new Error(`Failed to parse response JSON: ${err instanceof Error ? err.message : err}`));
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      clearTimeout(timeoutId);
+      reject(new Error(`Request failed: ${error instanceof Error ? error.message : error}`));
+    });
+
+    // Send the JSON data
+    req.write(jsonData);
+    req.end();
+  });
+}
+
 /*
 https://mtrbrgup.shelly.cloud/updates/system/dpkg.json
 https://mtrbrgup.shelly.cloud/updates/npm/latest
 
-apt-get install -y nodejs=22.13.1-1nodesource1 -y --allow-downgrades
+apt-get install -y nodejs=22.12.0-1nodesource1 -y --allow-downgrades
 apt-get install -y nodejs=22.14.0-1nodesource1 -y
 */
 /*
