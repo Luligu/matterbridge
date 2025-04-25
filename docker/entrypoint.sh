a35ce14f2d3a51043bed46ef38c8ac6d5c356b20
Luligu <target.epithet-0h@icloud.com>
2025-04-25 17:19:24 +0200

    Edge 3.0.0-edge.14

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,8 +4,19 @@ FLAG_FILE="/app/.initialized"
 
 if [ ! -f "$FLAG_FILE" ]; then
   echo "Welcome to the Matterbridge edge docker image."
+
+  echo "Updating the image:"
+  apt-get update && apt-get upgrade -y
+
+  echo "Installing bluetooth essentials:"
+  apt-get install -y --no-install-recommends \
+    bluetooth bluez libbluetooth-dev libudev-dev build-essential libcap2-bin \
+    python3
+  setcap 'cap_net_raw' "$(which node)"
+
   echo "Node.Js version:"
   node -v
+  
   echo "Npm version:"
   npm -v
 
