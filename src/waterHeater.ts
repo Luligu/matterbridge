c8387d6158ecdb79233f9242d69ac25d1b54da89
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-05-18 19:59:23 +0200

    Update waterHeater.ts
    Add PowerSourceWiredClusterServer

--- src/waterHeater.ts
+++ src/waterHeater.ts
@@ -11,6 +11,7 @@ export class WaterHeater extends MatterbridgeEndpoint {
     super(waterHeater, { uniqueStorageKey: `${name.replaceAll(' ', '')}-${serial.replaceAll(' ', '')}` }, true);
     this.createDefaultIdentifyClusterServer()
       .createDefaultBasicInformationClusterServer(name, serial, 0xfff1, 'Matterbridge', 0x8000, 'Matterbridge Water Heater')
+      .createDefaultPowerSourceWiredClusterServer()
       .createDefaultWaterHeaterManagementClusterServer()
       .createDefaultWaterHeaterModeClusterServer()
   }
