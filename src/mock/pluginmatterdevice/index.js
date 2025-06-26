1cce41170408675156176d70f74344a96247d8cd
Luligu <target.epithet-0h@icloud.com>
2025-06-26 15:55:51 +0200

    Add mode configuration to MatterbridgeEndpoint for mock platforms

--- src/mock/pluginmatterdevice/index.js
+++ src/mock/pluginmatterdevice/index.js
@@ -10,10 +10,9 @@ class MockPlatform extends MatterbridgeDynamicPlatform {
   async onStart(reason) {
     await this.ready;
     this.log.info(`Starting platform ${this.config.name}: ${reason ?? ''}`);
-    const device = new MatterbridgeEndpoint(pressureSensor, { uniqueStorageKey: 'MatterNodeDevice' })
+    const device = new MatterbridgeEndpoint(pressureSensor, { uniqueStorageKey: 'MatterNodeDevice', mode: 'matter' })
       .createDefaultBasicInformationClusterServer('Matter node device', '0x123456789', 0xfff1, 'Matterbridge', 0x8000, 'Matterbridge Matter Node Device')
       .addRequiredClusterServers();
-    device.serverMode = 'matter';
     await this.registerDevice(device);
     this.setSelectDevice('0x123456789', 'Matter node device');
     this.setSelectEntity('Pressure', 'Pressure', 'matter');
--- src/mock/pluginserverdevice/index.js
+++ src/mock/pluginserverdevice/index.js
@@ -10,10 +10,9 @@ class MockPlatform extends MatterbridgeAccessoryPlatform {
   async onStart(reason) {
     await this.ready;
     this.log.info(`Starting platform ${this.config.name}: ${reason ?? ''}`);
-    const device = new MatterbridgeEndpoint(pressureSensor, { uniqueStorageKey: 'ServerNodeDevice' })
+    const device = new MatterbridgeEndpoint(pressureSensor, { uniqueStorageKey: 'ServerNodeDevice', mode: 'server' })
       .createDefaultBasicInformationClusterServer('Server node device', '0x123456789', 0xfff1, 'Matterbridge', 0x8000, 'Matterbridge Server Node Device')
       .addRequiredClusterServers();
-    device.serverMode = 'server';
     await this.registerDevice(device);
     this.setSelectDevice('0x123456789', 'Server node device');
     this.setSelectEntity('Pressure', 'Pressure', 'matter');
