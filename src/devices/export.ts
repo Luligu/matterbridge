754ce7f05065da8caeaec5802c6e753e82fb7fd9
Luligu <target.epithet-0h@icloud.com>
2025-08-19 15:32:20 +0200

    Release 3.2.3

--- src/devices/export.ts
+++ src/devices/export.ts
@@ -1,4 +1,7 @@
-// Single class device types
+// Chapter 12. Robotic Device Types - Single class device types
+export * from './roboticVacuumCleaner.js';
+
+// Chapter 13. Appliances Device Types - Single class device types
 export * from './laundryWasher.js';
 export * from './laundryDryer.js';
 export * from './extractorHood.js';
@@ -8,10 +11,9 @@ export * from './oven.js';
 export * from './cooktop.js';
 export * from './refrigerator.js';
 
+// Chapter 14. Energy Device Types - Single class device types
 export * from './waterHeater.js';
 export * from './evse.js';
 export * from './solarPower.js';
 export * from './batteryStorage.js';
 export * from './heatPump.js';
-
-export * from './roboticVacuumCleaner.js';
