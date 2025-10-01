38af6b608d23af3ab8740d425276cbd79f0dba14
Luligu <target.epithet-0h@icloud.com>
2025-10-01 11:23:35 +0200

    Add prettier

---
+++ frontend/prettier.config.js
@@ -0,0 +1,23 @@
+// prettier.config.js
+
+// Config for Prettier
+
+export default {
+  printWidth: 250, // default 80
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true, // default false
+  quoteProps: 'preserve', // default 'as-needed'. Other 'consistent'
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'off', // default 'auto'
+  singleAttributePerLine: false,
+};
