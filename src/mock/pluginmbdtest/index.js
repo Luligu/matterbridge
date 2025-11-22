e9ea4c7ee9d82c2e0bb45d6f83ce88580334a547
Luligu <target.epithet-0h@icloud.com>
2025-11-22 09:45:18 +0100

    Add mock platform test plugins for Matterbridge

---
+++ src/mock/pluginmbdtest/index.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line n/no-missing-import
+import { MatterbridgeDynamicPlatform, MatterbridgeEndpoint, pressureSensor } from 'matterbridge';
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+export default function initializePlugin(matterbridge, log, config) {
+  return new MockPlatform(matterbridge, log, config);
+}
+
+class MockPlatform extends MatterbridgeDynamicPlatform {
+  async onStart(reason) {
+    await this.ready;
+    this.log.info(`Starting platform ${this.config.name}: ${reason ?? ''}`);
+    const device = new MatterbridgeEndpoint(pressureSensor, { id: 'TestPlugin' }).createDefaultBasicInformationClusterServer('Test plugin', '0x123456789', 0xfff1, 'Matterbridge', 0x8000, 'Matterbridge PressureSensor').addRequiredClusterServers();
+    await this.registerDevice(device);
+    this.setSelectDevice('0x123456789', 'Test plugin');
+    this.setSelectEntity('Pressure', 'Pressure', 'matter');
+  }
+  async onConfigure() {
+    await super.onConfigure();
+    this.log.info(`Configuring platform ${this.config.name}`);
+  }
+  async onShutdown(reason) {
+    await super.onShutdown(reason);
+    this.log.info(`Shutting down platform ${this.config.name}: ${reason ?? ''}`);
+  }
+}
