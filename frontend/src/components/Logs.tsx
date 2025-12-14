1807b81e3cc7ef49762a807951a0af47a3d78b49
Luligu <target.epithet-0h@icloud.com>
2025-12-14 18:57:17 +0100

    Add Log length: 100, 200, 500, 1000

--- frontend/src/components/Logs.tsx
+++ frontend/src/components/Logs.tsx
@@ -24,40 +24,52 @@ import { debug } from '../App';
 // const debug = true;
 
 function Logs(): React.JSX.Element {
+  // Contexts
+  const { logLength, logAutoScroll, setMessages, setLogFilterLevel: setContextLogFilterLevel, setLogFilterSearch: setContextLogFilterSearch, online, filterLogMessages } = useContext(WebSocketContext);
   // States
   const [logFilterLevel, setLogFilterLevel] = useState(localStorage.getItem('logFilterLevel') ?? 'info');
   const [logFilterSearch, setLogFilterSearch] = useState(localStorage.getItem('logFilterSearch') ?? '*');
-  const [logAutoScroll, setLogAutoScroll] = useState(localStorage.getItem('logAutoScroll') === 'false' ? false : true);
-  // Contexts
-  const { setMessages: setContextMessages, setLogFilterLevel: setContextLogFilterLevel, setLogFilterSearch: setContextLogFilterSearch, online, setLogAutoScroll: setContextAutoScroll, filterLogMessages } = useContext(WebSocketContext);
+  const [localLogLength, setLocalLogLength] = useState(logLength.current.toString());
+  const [localLogAutoScroll, setLocalLogAutoScroll] = useState(logAutoScroll.current);
 
-  const handleChangeLevel = (event: SelectChangeEvent) => {
-    setLogFilterLevel(event.target.value);
-    setContextLogFilterLevel(event.target.value);
-    filterLogMessages(event.target.value, logFilterSearch);
-    localStorage.setItem('logFilterLevel', event.target.value);
-    if (debug) console.log('handleChangeLevel called with value:', event.target.value);
+  const handleLogFilterLevelChange = (event: SelectChangeEvent) => {
+    const newValue = event.target.value;
+    setLogFilterLevel(newValue);
+    setContextLogFilterLevel(newValue);
+    filterLogMessages(newValue, logFilterSearch);
+    localStorage.setItem('logFilterLevel', newValue);
+    if (debug) console.log('handleLogFilterLevelChange called with value:', newValue);
   };
 
-  const handleChangeSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLogFilterSearch(event.target.value);
-    setContextLogFilterSearch(event.target.value);
-    filterLogMessages(logFilterLevel, event.target.value);
-    localStorage.setItem('logFilterSearch', event.target.value);
-    if (debug) console.log('handleChangeSearch called with value:', event.target.value);
+  const handleLogFilterSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    setLogFilterSearch(newValue);
+    setContextLogFilterSearch(newValue);
+    filterLogMessages(logFilterLevel, newValue);
+    localStorage.setItem('logFilterSearch', newValue);
+    if (debug) console.log('handleLogFilterSearchChange called with value:', newValue);
   };
 
-  const handleAutoScrollChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLogAutoScroll(event.target.checked);
-    setContextAutoScroll(event.target.checked);
+  const handleLogAutoScrollChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const checked = event.target.checked;
+    setLocalLogAutoScroll(checked);
+    logAutoScroll.current = checked;
     filterLogMessages(logFilterLevel, logFilterSearch);
-    localStorage.setItem('logAutoScroll', event.target.checked ? 'true' : 'false');
-    if (debug) console.log('handleAutoScrollChange called with value:', event.target.checked);
+    localStorage.setItem('logAutoScroll', checked ? 'true' : 'false');
+    if (debug) console.log('handleAutoScrollChange called with value:', checked);
+  };
+
+  const handleLogLengthChange = (event: SelectChangeEvent) => {
+    const newValue = event.target.value;
+    setLocalLogLength(newValue);
+    logLength.current = Number(newValue);
+    localStorage.setItem('logLength', newValue);
+    if (debug) console.log('handleLogLengthChange called with value:', newValue);
   };
 
   const handleClearLogsClick = () => {
     if (debug) console.log('handleClearLogsClick called');
-    setContextMessages([]);
+    setMessages([]);
   };
 
   if (debug) console.log('Logs rendering...');
@@ -74,7 +86,7 @@ function Logs(): React.JSX.Element {
             <InputLabel id='select-level' style={{ color: 'var(--div-text-color)' }}>
               Filter log by level:
             </InputLabel>
-            <Select style={{ height: '30px', backgroundColor: 'var(--main-bg-color)' }} labelId='select-level' id='debug-level' value={logFilterLevel} onChange={handleChangeLevel}>
+            <Select style={{ height: '30px', backgroundColor: 'var(--main-bg-color)' }} labelId='select-level' id='debug-level' value={logFilterLevel} onChange={handleLogFilterLevelChange}>
               <MenuItem value='debug'>Debug</MenuItem>
               <MenuItem value='info'>Info</MenuItem>
               <MenuItem value='notice'>Notice</MenuItem>
@@ -87,28 +99,39 @@ function Logs(): React.JSX.Element {
             <InputLabel id='search' style={{ color: 'var(--div-text-color)' }}>
               Filter log by text:
             </InputLabel>
-            <TextField
-              style={{ width: '210px' }}
-              size='small'
-              id='logsearch'
-              variant='outlined'
-              value={logFilterSearch}
-              onChange={handleChangeSearch}
-              slotProps={{
-                input: {
-                  style: {
-                    height: '30px',
-                    padding: '0 0px',
-                    backgroundColor: 'var(--main-bg-color)',
+            <Tooltip title='Use /text/ for case-insensitive regex search'>
+              <TextField
+                style={{ width: '210px' }}
+                size='small'
+                id='logsearch'
+                variant='outlined'
+                value={logFilterSearch}
+                onChange={handleLogFilterSearchChange}
+                slotProps={{
+                  input: {
+                    style: {
+                      height: '30px',
+                      padding: '0 0px',
+                      backgroundColor: 'var(--main-bg-color)',
+                    },
                   },
-                },
-              }}
-            />
+                }}
+              />
+            </Tooltip>
           </div>
-          <FormControlLabel control={<Checkbox checked={logAutoScroll} onChange={handleAutoScrollChange} />} label='Auto scroll' style={{ color: 'var(--div-text-color)' }} />
+          <FormControlLabel control={<Checkbox checked={localLogAutoScroll} onChange={handleLogAutoScrollChange} />} label='Auto scroll' style={{ color: 'var(--div-text-color)' }} />
         </div>
         {/* Clear Button */}
-        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', gap: '10px' }}>
+          <InputLabel id='select-size-label' style={{ color: 'var(--div-text-color)' }}>
+            Log length:
+          </InputLabel>
+          <Select style={{ height: '30px', backgroundColor: 'var(--main-bg-color)' }} labelId='select-size-label' id='select-size' value={localLogLength} onChange={handleLogLengthChange}>
+            <MenuItem value={100}>100</MenuItem>
+            <MenuItem value={200}>200</MenuItem>
+            <MenuItem value={500}>500</MenuItem>
+            <MenuItem value={1000}>1000</MenuItem>
+          </Select>
           <Tooltip title='Clear the logs'>
             <Button onClick={handleClearLogsClick} endIcon={<DeleteForever />} style={{ color: 'var(--main-button-color)', backgroundColor: 'var(--main-button-bg-color)', height: '30px' }}>
               Clear
