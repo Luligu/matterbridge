c7efa5e671982dec48c013eca074bffd292d34b3
Luligu <target.epithet-0h@icloud.com>
2026-01-15 11:06:35 +0100

    Refactor exports

--- src/matter/export.ts
+++ src/matter/export.ts
@@ -1,3 +1,4 @@
+// @matter
 export * from '@matter/main';
 export { AttributeElement, ClusterElement, ClusterModel, CommandElement, EventElement, FieldElement, MatterDefinition } from '@matter/main/model';
 export { MdnsService, Val } from '@matter/main/protocol';
