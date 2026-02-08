0f4357e86eea1664b66ce8b2b425d8a2046b5bda
Luligu <target.epithet-0h@icloud.com>
2026-02-08 06:13:44 +0000

    Add @matterbridge/core

---
+++ packages/core/prettier.config.js
@@ -0,0 +1,23 @@
+// prettier.config.js
+
+// Config for Prettier
+
+export default {
+  printWidth: 180, // default 80
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true, // default false
+  quoteProps: 'consistent', // default 'as-needed'
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'off', // default 'auto'
+  singleAttributePerLine: false,
+};
