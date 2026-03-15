c65f353b3fcbb520622f02d0aaa542619ba87add
Luligu <target.epithet-0h@icloud.com>
2026-03-15 18:02:43 +0100

    Add cli tests

--- packages/core/src/cli.error.test.ts
+++ packages/core/src/cli.error.test.ts
@@ -58,7 +58,7 @@ describe('Matterbridge', () => {
   });
 
   it('should start matterbridge and fail', async () => {
-    process.argv = ['node', './cli.js', '-frontend', '0', '-logger', 'debug', '-matterlogger', 'debug'];
+    process.argv = ['node', './cli.js', '-frontend', '0', '-logger', 'debug', '-matterlogger', 'debug', '--no-ansi'];
     const cli = await import('./cli.js');
     await new Promise((resolve) => setTimeout(resolve, 100));
     expect(cli.instance).toBeUndefined();
